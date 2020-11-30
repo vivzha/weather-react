64c8a19e94f2fb087e63ce8c3a9e92e8934f1943
vivzha <vzha215@aucklanduni.ac.nz>
2020-11-30 17:36:14 +1300

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from "react";
-import Weather from "./Weather";
-
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello from React</h1>
-        <Weather city="Paris" />
-      </header>
-    </div>
-  );
+  return <div className="App">Weather App</div>;
 }
 
 export default App;
