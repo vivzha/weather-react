4bb36a41faefbb0c42c29d9d9ecb939734b74363
vivzha <vzha215@aucklanduni.ac.nz>
2020-11-30 18:01:29 +1300

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is created by Vivian Zhang and is{" "}
-        <a href="https://github.com/vivzha/weather-react" target="_blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="Container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by Vivian Zhang and is{" "}
+          <a href="https://github.com/vivzha/weather-react" target="_blank">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
