68eb7f730605911480af22da6f9c70816aa588c6
vivzha <vzha215@aucklanduni.ac.nz>
2020-11-30 21:52:34 +1300

    Added basic styling

---
+++ src/Weather.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+              autoFocus="on"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+              alt="Light rain showers"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">℃</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation:15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 15km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
