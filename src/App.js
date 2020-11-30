f20c21ca894c4bce34b5ee76117bd50ef6ce70d7
vivzha <vzha215@aucklanduni.ac.nz>
2020-11-30 17:50:27 +1300

    Added the Github Repo URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is created by Vivian Zhang and is{" "}
+        <a href="https://github.com/vivzha/weather-react" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
